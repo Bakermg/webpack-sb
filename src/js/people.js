1635c7ed222d1aaea884f4bb91edec6eecbda136
Mike Baker <mike@bakerwebco.com>
2017-05-01 16:15:18 -0400

    First commit of webpack modules sandbox and boilerplate

---
+++ src/js/people.js
@@ -0,0 +1,13 @@
+// function getHello() {
+// 	return 'Hello There!'
+// }
+
+// module.exports = getHello();
+
+let people = [
+	{name: 'Mike Baker'},
+	{name: 'Brianna'},
+	{name: 'Gretchen'}
+];
+
+module.exports = people;
